927512c7c50229f6eb60c9ecda044c264a5da3ba
jimlai <athkatlaca@gmail.com>
2015-11-06 00:32:05 +0800

    Update ReverseInteger.swift

--- Algorithms/ReverseInteger.swift
+++ Algorithms/ReverseInteger.swift
@@ -1,54 +1,22 @@
-//
-//  ReverseInteger.swift
-//  LeetCode
-//
-//  Created by Lex Tang on 4/17/15.
-//  Copyright (c) 2015 Lex Tang. All rights reserved.
-//
-
-/*
-Reverse digits of an integer.
-
-Example1: x = 123, return 321
-Example2: x = -123, return -321
-
-*******
-
-Have you thought about this?
-
-Here are some good questions to ask before coding. Bonus points for you if you have already thought through this!
-
-If the integer's last digit is 0, what should the output be? ie, cases such as 10, 100.
-
-Did you notice that the reversed integer might overflow? Assume the input is a 32-bit integer, then the reverse of 1000000003 overflows. How should you handle such cases?
-
-For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
-*/
-
-import Foundation
-
-
-extension Int {
+func reverseInt(var num:Int) -> Int {
     
-    /**
-    This Swift method is imitated from the C++ solution:
-    https://github.com/haoel/leetcode/blob/master/algorithms/reverseInteger/reverseInteger.cpp
+    var signed = false
+    signed = num >= 0 ? false : true
     
-    :returns: The reversed Int
-    */
-    func reverse() -> Int {
-        var y = 0, n = 0
-        var x = self
-        while x != 0 {
-            n = x % 10
-            // Check over/underflow
-            if y > (Int(INT32_MAX) - n)/10 {
-                return 0
-            }
-            y = y * 10 + n
-            x /= 10
-        }
-        return y
+    if signed == true {
+        num = -num
     }
     
+    var rev = [String(num)].map() {
+        String($0.characters.reverse())
+    }
+    
+    
+    print(rev[0])
+
+    
+    if signed == true {
+        rev[0] = "-\(rev[0])"
+    }
+    return Int(rev[0])!
 }
