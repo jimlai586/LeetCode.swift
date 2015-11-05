524cd2f51bf6e735770311f97d7358958b2d8c34
jimlai <athkatlaca@gmail.com>
2015-11-06 00:38:08 +0800

    Update RemoveDuplicatesFromSortedArray.swift

--- Algorithms/RemoveDuplicatesFromSortedArray.swift
+++ Algorithms/RemoveDuplicatesFromSortedArray.swift
@@ -1,45 +1,16 @@
-//
-//  RemoveDuplicatesFromSortedArray.swift
-//  LeetCode
-//
-//  Created by Lex Tang on 4/21/15.
-//  Copyright (c) 2015 Lex Tang. All rights reserved.
-//
-
-/*
-Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
-
-Do not allocate extra space for another array, you must do this in place with constant memory.
-
-For example,
-Given input array A = [1,1,2],
-
-Your function should return length = 2, and A is now [1,2].
-*/
-
-import Foundation
-
-
-extension NSMutableArray {
-    
-    // O(n)
-    func removeDuplicates() -> Int {
-        // In case there is nothing to do
-        if self.count == 0 {
-            return 0
+func removeDuplicates(var dup: [Int]) {
+    var p = 1
+    var ori = dup[0]
+    for i in 1 ..< dup.count {
+        if dup[i] != ori {
+            ori = dup[i]
+            dup[p++] = dup[i]
+            
         }
-        
-        var index = 0
-        for var i = 1; i < self.count; i++ {
-            let l: AnyObject = self.objectAtIndex(index)
-            let r: AnyObject = self.objectAtIndex(i)
-            if !l.isEqual(r) {
-                self.replaceObjectAtIndex(++index, withObject: r)
-            }
+        else {
+            continue
         }
-        
-        // Remove the remaining
-        self.removeObjectsInRange(NSRange(location: index + 1, length: self.count - index - 1))
-        return index + 1
     }
+    print(dup)
+    
 }
